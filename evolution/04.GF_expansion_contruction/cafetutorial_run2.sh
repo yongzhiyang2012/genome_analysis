b7dcc11e6acc7efb49f584981887a00092d873d0
yongzhiyang <yangyongzhi2008@gmail.com>
2017-06-10 22:34:06 +0800

    cafe

---
+++ evolution/04.GF_expansion_contruction/cafetutorial_run2.sh
@@ -0,0 +1,7 @@
+#!/home/share/users/yangyongzhi2012/tools/cafe/CAFE/release/cafe
+
+load -i large_filtered_cafe_input.txt -t 4 -l reports/log_run2.txt
+tree ((((((((Oam:5.489859,She:5.489859):4.999583,Goa:10.489442):13.516361,Cow:24.005802):38.721828,Pig:62.727630):19.175412,Hor:81.903042):2.544888,Dog:84.447930):11.519883,Hom:95.967813):62.740158,Opo:158.707971)
+lambda -l 0.00171099260363 
+report reports/report_run2
+
