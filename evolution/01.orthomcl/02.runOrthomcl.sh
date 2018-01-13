187ccac5c83b122bab3064b4d61a1ea06d8ebb86
yongzhiyang <yangyongzhi2008@gmail.com>
2018-01-13 20:02:58 +0800

    fix

--- evolution/01.orthomcl/02.runOrthomcl.sh
+++ evolution/01.orthomcl/02.runOrthomcl.sh
@@ -1,8 +1,9 @@
 mysql -u root < mysql.setting.template
+orthomclInstallSchema orthomcl.config.template
+
 orthomclFilterFasta compliantFasta/ 10 20
 makeblastdb -in goodProteins.fasta -dbtype prot
 blastp -db goodProteins.fasta -query goodProteins.fasta -out all-all.blastp.out -evalue 1e-5 -outfmt 6 -num_threads 24
-orthomclInstallSchema orthomcl.config.template
 orthomclBlastParser all-all.blastp.out compliantFasta > similarSequences.txt
 perl -p -i -e 's/0\t0/1\t-181/' similarSequences.txt
 orthomclLoadBlast orthomcl.config.template similarSequences.txt
