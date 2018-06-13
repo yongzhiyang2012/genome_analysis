e12bde419cfbb94764d833782a6a97d86736807e
yongzhiyang <yangyongzhi2008@gmail.com>
2018-06-13 20:40:12 +0800

    update

--- evolution/06.ltr.insert.time/04.classifer.pl
+++ evolution/06.ltr.insert.time/04.classifer.pl
@@ -1,8 +1,7 @@
 use strict;
 use warnings;
 
-my $RepeatModeler=shift or die "perl $0 <path-to-RepeatModeler> <consensi-lib-file>;\nthe path should be the dir contained RepeatClassifier\n";
-my $consensi=shift or die "perl $0 <path-to-RepeatModeler> <consensi-lib-file>;\nthe path should be the dir contained RepeatClassifier\n";
+my $RepeatModeler=shift or die "perl $0 <path-to-RepeatModeler>;\nthe path should be the dir contained RepeatClassifier\n";
 
 my $in="genome.ltrfinder.seq.fa";
 die "give the right inpu genome.ltrfinder.seq.fa\n" if ! -e "$in";
@@ -18,7 +17,7 @@ while (<./*part-*>) {
     chomp;
     if ($_=~/^(\S+\.part-(\d+))/){
         `mkdir $2 ; mv $1* $2 `;
-        print O "cd $in.split/$2 ; $RepeatModeler/RepeatClassifier -consensi $consensi -engine abblast $1 ; cd ../../\n";
+        print O "cd $in.split/$2 ; $RepeatModeler/RepeatClassifier -engine abblast -consensi $1.fa ; cd ../../\n";
     }
 }
 
